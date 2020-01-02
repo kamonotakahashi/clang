f8afe036354850b87d14f17b7076dbcb4a36ff5b
kamonotakahashi <kamonotakahashi@gmail.com>
2020-01-02 20:19:31 +0900

    普通のそーと（N^2） リニアサーチ手法

--- sort/linear_search_method.c
+++ sort/linear_search_method.c
@@ -3,16 +3,62 @@
 #define ASC 1
 #define DESC 2
 
-int length(int *data) {
-    int count = 0;
-    while(*data != -1) {
-        ++data;
-        count++;
+void sort(int *data, int length, int types);
+
+int main(void) {
+    int data[] = {53,123,56,123,522};
+    int length = sizeof(data)/sizeof(data[0]);
+
+    //昇順
+    sort(data, length, ASC);
+    for(int i = 0; i < length; i++) {
+        if(i != length - 1) {
+            printf("%d,", data[i]);
+        } else {
+            printf("%d\n", data[i]);
+        }
     }
-    return count;
+
+    //降順
+    sort(data, length, DESC);
+    for(int i = 0; i < length; i++) {
+        if(i != length - 1) {
+            printf("%d,", data[i]);
+        } else {
+            printf("%d\n", data[i]);
+        }
+    }
+
+    return 0;
 }
 
-int main (void) {
-    int data[] = {51,3,2,1,6,-1};
-    printf("%d", length(data) );
+void sort(int *data, int length, int types) {
+    int max;
+    int pos;
+
+    if(types==ASC) {
+        for(int i = length; i < length; i--) {
+            max = 0;
+            for(int x = i; x < length; x--) {
+                if(max < data[x]) {
+                    pos = x;
+                    max = data[x];
+                }
+            }
+            data[pos] = data[i];
+            data[i] = max;
+        }
+    } else {
+        for(int i = 0; i < length; i++) {
+            max = 0;
+            for(int x = i; x < length; x++) {
+                if(max < data[x]) {
+                    pos = x;
+                    max = data[x];
+                }
+            }
+            data[pos] = data[i];
+            data[i] = max;
+        }
+    }
 }
